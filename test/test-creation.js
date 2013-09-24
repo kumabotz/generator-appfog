8bf21591bdbe60ea273083e42138bf7e976ed3cc
Tery Lim <kumabotz@users.noreply.github.com>
2013-09-25 01:59:02 +0800

    Create test-creation.js

---
+++ test/test-creation.js
@@ -0,0 +1,10 @@
+/* global describe, beforeEach, it */
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+var helper = require('yeoman-generator').test;
+
+describe('appfog generator', function() {
+  // TODO
+});
