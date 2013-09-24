41d454aca202a7368e2c1b9eef7c074ebb6e604c
Tery Lim <kumabotz@users.noreply.github.com>
2013-09-25 01:56:09 +0800

    Update test-load.js

--- test/test-load.js
+++ test/test-load.js
@@ -1,2 +1,11 @@
 /* global describe, beforeEach, it */
 'use strict';
+
+var assert = require('assert');
+
+describe('appfog generator', function() {
+  it('can be imported without blowing up', function() {
+    var app = require('../app');
+    assert(app !== undefined);
+  })
+});
