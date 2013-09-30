170e0d92e01bee00b3a9ed68e862faa618ae27cb
kumabotz <kumabotz@gmail.com>
2013-09-30 08:26:43 +0000

    Stub

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,66 +1,38 @@
-/* global describe, beforeEach, it */
+/*global describe, beforeEach, it*/
 'use strict';
 
-var path = require('path');
-var fs = require('fs');
+var path    = require('path');
 var helpers = require('yeoman-generator').test;
 
-describe('appfog generator', function() {
-  beforeEach(function(done) {
-    this.tmpDir = path.join(__dirname, 'temp');
-    helpers.testDirectory(this.tmpDir, function(err) {
+
+describe('appfog generator', function () {
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
       if (err) {
         return done(err);
       }
-      
-      fs.writeFileSync(path.join(this.tmpDir, 'package.json'), JSON.stringify({
-        name: 'testproj',
-        version: '1.3.7'
-      }));
-      
-      // this.app = helpers.createGenerator('heroku:app', [
-      //   '../../app'
-      // ]);
+
+      this.app = helpers.createGenerator('appfog:app', [
+        '../../app'
+      ]);
       done();
     }.bind(this));
   });
-  
-  it('creates expected files', function(done) {
+
+  it('creates expected files', function (done) {
     var expected = [
-      // add files you expected to exist here
-      'appfog/Procfile',
-      'appfog/server.js',
-      ['appfog/distpackage.json', /"name": "testproj"/]
+      // add files you expect to exist here.
+      '.jshintrc',
+      '.editorconfig'
     ];
-    
-    // helpers.mockPrompt(this.app, {
-    //   'distGit': 'Y'
-    // });
-    
-    // override the system check
-    // this.app.checkInstallation = function() {};
-    
-    // this.app.options['skip-install'] = true;
-    // this.app.run({}, function() {
-    //   helpers.assertFiles(expected);
-    //   done();
-    // });
-  });
-  
-  it('works w/o package.json', function(done) {
-    // helpers.mockPrompt(this.app, {
-    //   'distGit': 'N'
-    // });
-    
-    var pkgPath = path.join(this.tmpDir, 'package.json');
-    fs.unlinkSync(pkgPath);
-    
-    // override the system check
-    // this.app.checkInstallation = function() {};
-    
-    // this.app.options['skip-install'] = true;
-    // this.app.run({}, function() {
-    //   done();
-    // });
+
+    helpers.mockPrompt(this.app, {
+      'someOption': true
+    });
+    this.app.options['skip-install'] = true;
+    this.app.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
   });
 });
