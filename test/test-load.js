fdbb2b769f42b86378fa417d81b84104050c3595
kumabotz <kumabotz@gmail.com>
2013-09-30 08:15:56 +0000

    Add missing semicolon

--- test/test-load.js
+++ test/test-load.js
@@ -1,11 +1,11 @@
-/* global describe, beforeEach, it */
+/*global describe, beforeEach, it*/
 'use strict';
 
-var assert = require('assert');
+var assert  = require('assert');
 
-describe('appfog generator', function() {
-  it('can be imported without blowing up', function() {
+describe('appfog generator', function () {
+  it('can be imported without blowing up', function () {
     var app = require('../app');
     assert(app !== undefined);
-  })
+  });
 });
